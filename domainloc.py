b9d17e4a4f053427d8290cb19c32ddd622984817
naseeeeef <32449@yenepoya.edu.in>
2025-04-08 14:52:12 +0530

    Create domainloc.py

---
+++ domainloc.py
@@ -0,0 +1,11 @@
+import socket
+import requests as req
+import json
+
+
+url = 'google.com'
+ip = socket.gethostbyname(url)
+req = req.get(f'https://ipinfo.io/{ip}/json')
+data = json.loads(req.text)
+print (f'ip of {url} is {ip}\n\n')
+print (f'location of the server is {data['loc']}')
